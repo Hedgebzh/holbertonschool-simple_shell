02bb01bf9f1a73669917d6f21d18114cdea9be43
william0863 <granger.w35@gmail.com>
2022-07-26 10:12:57 +0200

    betty style

--- test.c
+++ test.c
@@ -2,66 +2,81 @@
 
 void read_command(char cmd[], char par[])
 {
-    char line[1024];
-    int count = 0, i = 0, j = 0;
-    chararray[100], pch;
+	char line[1024];
+	int count = 0, i = 0, j = 0;
+	chararray[100], pch;
 
-    for (;;) {
-        int c = fgetc(stdin);
-        line[count++] = (char) c;
-        if (c == '\n') {
-            break;
-        }
-        if (count == 1) {
-            return;
-        }
-        pch = strtok(line, "\n");
+	/* Read one line */
+	for (;;)
+	{
+		int c = fgetc(stdin);
+		line[count++] = (char) c;
+		if (c == '\n')
+		{
+			break;
+		}
+		if (count == 1)
+		{
+			return;
+		}
+		pch = strtok(line, "\n");
 
-        while (pch != NULL)
-        {
-            array[i++] = strdup(pch);
-            pch = strtok(NULL, "\n");
-        }
+	/* Parse the line into words */
+		while (pch != NULL)
+			{
+			array[i++] = strdup(pch);
+			pch = strtok(NULL, "\n");
+			}
+	/* First word is the command */
+	strcpy(cmd, array[0]);
 
-        strcpy(cmd, array[0]);
-
-        for (int j = 0; j < i; j++) {
-            par[j] = array[j];
-            par[i] = NULL;
-        }
-    }
+	/* others are parameters */
+		for (int j = 0; j < i; j++)
+		{
+			par[j] = array[j];
+			par[i] = NULL; // NULL terminates the parameter list
+		}
+	}
 }
 
 void type_prompt()
 {
-    static int first_time = 1;
-    if (first_time) {
-        const char CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
-        write(STDERR_FILENO, CLEAR_SCREEN_ANSI, 12);
-        first_time = 0;
-    }
+	static int first_time = 1;
+	if (first_time) // clear screen for the first time
+	{
+		const char CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+		write(STDERR_FILENO, CLEAR_SCREEN_ANSI, 12);
+		first_time = 0;
+	}
 
-    printf("#");
+	printf("#"); // display prompt
 }
 
 int main()
 {
-    char cmd[100], command[100], parameters[20];
+	char cmd[100], command[100], parameters[20];
 
-    charenvp[] = { (char *) "PATH=C:\Windows\System32", 0 };
-    while(1) {
-        type_prompt();
-        read_command(command, parameters);
-        if (fork() != 0) {
-            wait(NULL);
-        } else {
-            strcpy(cmd, "C:\Windows\System32");
-            strcpy(cmd, command);
-            execvp(cmd, parameters, envp);
-        }
-        if (strcmp(command, "exit") == 0) {
-            break;
-        }
-    }
-    return 0;
+	/* environment variable */
+	char *envp[] = { (char *) "PATH=/bin", 0 };
+	/* infinite loop */
+	while(1)
+	{
+		type_prompt(); /* display prompt on screen */
+		read_command(command, parameters); /* read input from terminal */
+		if (fork() != 0) // parent
+		{
+			wait(NULL); // wait for child
+		}
+		else
+		{
+			strcpy(cmd, "/bin/");
+			strcpy(cmd, command);
+			execvp(cmd, parameters, envp); // execute command
+		}
+		if (strcmp(command, "exit") == 0)
+		{
+		break;
+		}
+	}
+	return 0;
 }
