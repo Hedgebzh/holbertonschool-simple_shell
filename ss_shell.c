80c8697aa840eb688b4a244e4d06e9901cc54788
william0863 <granger.w35@gmail.com>
2022-07-27 17:20:11 +0200

    Finishing super simple shell

---
+++ ss_shell.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main(void)
+{
+char *str;
+int kirikou, slip;
+size_t len;
+char *array[2];
+
+str = malloc(sizeof(char) * 1024);
+	while (1)
+		{
+			printf("$ ");
+			getline (&str, &len, stdin);
+			str[strlen(str) - 1] = '\0';
+			array[0] = str;
+			array[1] = NULL;
+kirikou = fork();
+if (kirikou == -1)
+{
+	perror("Boloss corrige-moi ça et vite fait!");
+	return(1);
+}
+if (kirikou == 0)
+	execve (array[0], array, NULL);
+			wait (&slip);
+		}
+		return(EXIT_SUCCESS);
+}
