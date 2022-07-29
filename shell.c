8cefcf5ead707e12b6f50bff992d749db8732056
Hedgebzh <axel.gore35@gmail.com>
2022-07-29 16:53:49 +0200

    fix ! by 0

--- shell.c
+++ shell.c
@@ -4,14 +4,19 @@ int main(void)
 {
 	char *cmd, *ptr, *argv[256];
 	size_t len;
-	int child_p, status, i;
+	int child_p, i;
 
 	while(1)
 	{
 	getline(&cmd, &len, stdin);
 	cmd[strlen(cmd) - 1] = '\0';
 
-	ptr = strtok(cmd, " ");
+	if(strcmp("", cmd) == 0)
+	continue;
+	if(strcmp("exit", cmd) == 0)
+	break;
+
+	ptr = strtok(cmd, " "); /* divise la commande pour les arguments */
 	i = 0;
 	while(ptr != NULL)
 	{
@@ -20,11 +25,12 @@ int main(void)
 		ptr = strtok(NULL, " ");
 	}
 
-	if(!strcmp("&", argv[i-1]))
+	if(strcmp("&", argv[i-1]) == 0) /* check pourquoi strcmp */
 	{
 	argv[i-1] = NULL;
 	argv[i] = "&";
-	}else
+	}
+	else
 	{
 	argv[i] = NULL;
 	}
@@ -39,7 +45,7 @@ int main(void)
 	{
 		execvp(argv[0], argv);
 	}
-	wait(&status);
+	wait(NULL);
 	}
 	return (0);
 }
