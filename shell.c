82e83a59fda9b35a8ff6daa9563118a90837af3a
Hedgebzh <105491625+Hedgebzh@users.noreply.github.com>
2022-08-02 16:21:35 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,44 +2,49 @@
 
 int main(void)
 {
-	char *cmd, *ptr, *argv[256];
+	char *cmd, *ptr, *argv[1000];
 	size_t len;
-	int child_p, i;
+	pid_t child_p;
+	int i;
 
-	cmd = malloc(sizeof (char *) * 1000);
+	cmd = malloc(sizeof(char *) * 1000);
 
 	while (1)
 	{
-		getline(&cmd, &len, stdin); /* recuperation de l'input */
+		if (getline(&cmd, &len, stdin) == '\0')
+		break; /* recuperation de l'input */
+
 		cmd[strlen(cmd) - 1] = '\0'; /* suppresion du retour à la ligne */
 
-	if (strcmp("", cmd) == 0)
-	continue;
-	if (strcmp("exit", cmd) == 0)
-	break;
+		if (strcmp("", cmd) == 0)
+		continue;
 
-	ptr = strtok(cmd, " "); /* divise la commande pour les arguments */
-	i = 0;
-	while (ptr != NULL)
-	{
-		argv[i] = ptr;
-		i++;
-		ptr = strtok(NULL, " ");
-	}
+		if (strcmp("exit", cmd) == 0)
+		break;
 
-	child_p = fork();
+		ptr = strtok(cmd, " "); /* divise la commande pour les arguments */
+		i = 0;
+		while (ptr != NULL)
+		{
+			argv[i] = ptr;
+			i++;
+			ptr = strtok(NULL, " ");
+		}
 
-	if (child_p == -1)
-	{
-		perror("Error");
-		return (1);
+		child_p = fork();
+
+		if (child_p == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+		if (child_p == 0)
+		{
+			execvp(argv[0], argv);
+		}
+		wait(NULL);
 	}
-	if (child_p == 0)
-	{
-		execvp(argv[0], argv);
-	}
-	wait(NULL);
-	}
-	free (cmd);
-	return (0);
+	free(cmd);
+
+	exit(EXIT_SUCCESS);
 }
