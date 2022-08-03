f32cf403102e6961cce6b2b48927895f00e14888
W <granger.w35@gmail.com>
2022-08-03 17:08:20 +0200

    update betty

--- shell.c
+++ shell.c
@@ -1,28 +1,28 @@
 #include "shell.h"
 
 #define	MAX_COMMAND_LENGTH 1000
-#define	MAX_NUMBER_OF_PARAMS 1000
+#define	MAX_NUMBER_OF_PARAMS 100
 
 int main(void)
 {
 	char cmd[MAX_COMMAND_LENGTH + 1];
 	char *params[MAX_NUMBER_OF_PARAMS + 1];
 
-	while(1) 
+while(1)
 	{
 
 		if (fgets(cmd, sizeof(cmd), stdin) == NULL) break;
 
-		if(cmd[strlen(cmd)-1] == '\n') 
+		if (cmd[strlen(cmd)-1] == '\n')
 		{
 			cmd[strlen(cmd)-1] = '\0';
 		}
 
 		parseCmd(cmd, params);
 
-		if(strcmp(params[0], "exit") == 0) break;
+		if (strcmp(params[0], "exit") == 0) break;
 
-		if(executeCmd(params) == 0) break;
+		if (executeCmd(params) == 0) break;
 	}
 	return(0);
 }
@@ -32,10 +32,10 @@ void parseCmd(char *cmd, char **params)
 {
 	int i;
 
-	for(i = 0; i < MAX_NUMBER_OF_PARAMS; i++)
+	for (i = 0; i < MAX_NUMBER_OF_PARAMS; i++)
 	{
 		params[i] = strsep(&cmd, " ");
-		if(params[i] == NULL) break;
+		if (params[i] == NULL) break;
 	}
 }
 
@@ -43,24 +43,23 @@ int executeCmd(char **params)
 {
 	pid_t pid = fork();
 
-	if(pid == -1) 
-	{
-		perror("fork: error");
-		return(1);
+if (pid == -1) {
+		perror ("fork: error");
+		return (1);
 	}
 
-	else if (pid == 0)
+else if (pid == 0)
 	{
-		execvp(params[0], params);
+		execvp (params[0], params);
 
-		perror("shell: error");
+		perror ("shell: error");
 	}
 
-	else 
+else
 	{
 		int childStatus;
-		waitpid(pid, &childStatus, 0);
-		return(1);
+		waitpid (pid, &childStatus, 0);
+		return (1);
 	}
 	return(0);
 }
